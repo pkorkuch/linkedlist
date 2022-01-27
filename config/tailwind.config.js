38dc2a18221bbf31ef4cca62657160378c18ad67
Patrick Korkuch <patrick@korkuch.dev>
2022-01-26 21:31:52 -0500

    Install and initialize Tailwind

---
+++ config/tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Josefin Sans"', ...defaultTheme.fontFamily.sans],
+        serif: ['"Josefin Slab"', ...defaultTheme.fontFamily.serif]
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
