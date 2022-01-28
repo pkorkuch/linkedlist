cfcda384a52a1d9241ab5c4f1824e54b853bf476
Patrick Korkuch <patrick@korkuch.dev>
2022-01-27 20:20:14 -0500

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'LinkedList'
+    return base_title if page_title.empty?
+
+    "#{page_title} | #{base_title}"
+  end
 end
