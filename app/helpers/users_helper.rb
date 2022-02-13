612fb77ba8d7c96a8734b0bd9a00d7bff3197685
Patrick Korkuch <patrick@korkuch.dev>
2022-02-12 19:45:42 -0500

    Add size option to Gravatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,13 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = {})
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5.hexdigest(user.email.strip.downcase)
-    gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?r=g&d=mp"
-    image_tag(gravatar_url, alt: "Profile picture of user #{user.name}", class: "#{options[:class]} gravatar")
+
+    # Gravatar URL params:
+    # r=g — only allow gravatars with a 'G' rating
+    # d=mp — if a gravatar does not exist for the given email hash, return a 'gray silhouette' avatar
+    # s=SIZE — resize the avatar to a SIZExSIZE pixel square
+    gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?r=g&d=mp&s=#{options[:size]}"
+    image_tag(gravatar_url, alt: "Profile picture of user #{user.name}", class: "#{options[:class]} gravatar".strip)
   end
 end
