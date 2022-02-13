3a50b8e2bbe6f73d723a8a28b22e64e162acbf8a
Patrick Korkuch <patrick@korkuch.dev>
2022-02-12 19:46:43 -0500

    Add seed for fake users in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.development?
+  # Create a main sample user.
+  User.create!(name: 'Example User',
+               email: 'user@example.org',
+               password: 'password',
+               password_confirmation: 'password')
+
+  # Create an admin sample user.
+  User.create!(name: 'Admin',
+               email: 'admin@example.org',
+               password: 'password',
+               password_confirmation: 'password',
+               admin: true)
+
+  # Generate additional users.
+  99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n + 1}@example.org"
+    password = 'password'
+    User.create!(name: name,
+                 email: email,
+                 password: password,
+                 password_confirmation: password)
+  end
+end
