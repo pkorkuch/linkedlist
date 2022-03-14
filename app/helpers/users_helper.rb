58f5dfcbd9a216892fea55553834162bb52572ac
Patrick Korkuch <patrick@korkuch.dev>
2022-03-13 23:46:58 -0400

    Add sizing classes to gravatar helper.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,6 +8,7 @@ module UsersHelper
     # d=mp — if a gravatar does not exist for the given email hash, return a 'gray silhouette' avatar
     # s=SIZE — resize the avatar to a SIZExSIZE pixel square
     gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?r=g&d=mp&s=#{options[:size]}"
-    image_tag(gravatar_url, alt: "Profile picture of user #{user.name}", class: "#{options[:class]} gravatar".strip)
+    image_tag(gravatar_url, alt: "Profile picture of user #{user.name}",
+                            class: "h-[#{options[:size]}px] w-[#{options[:size]}px] #{options[:class]} gravatar".strip)
   end
 end
