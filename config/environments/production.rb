2d2d5b3ee4ed33e2e9607789f6a768fd4366f042
Patrick Korkuch <patrick@korkuch.dev>
2022-10-06 23:58:42 -0400

    Update account activaton email styling and default URL options for Action Mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,9 +73,9 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   config.action_mailer.default_options = {
-    from: 'noreply@korkuch.dev'
+    from: 'noreply@linkedlist.korkuch.dev'
   }
-  config.action_mailer.default_url_options = { host: 'korkuch.dev', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'linkedlist.korkuch.dev', protocol: 'https' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
