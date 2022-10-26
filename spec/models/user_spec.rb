8127e88d2591553612f0c1c07e0d4571978d3eef
Patrick Korkuch <patrick@korkuch.dev>
2022-10-25 22:56:58 -0400

    Fix user spec for password validation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
   context 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password).allow_nil }
+    it { should validate_presence_of(:password) }
     it { should have_secure_password }
     it { should validate_length_of(:name).is_at_most(255) }
     it { should validate_length_of(:email).is_at_most(255) }
